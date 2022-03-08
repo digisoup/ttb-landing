a9efa9667f39c96a8a800a3186e565de2aaaae94
chefbrams <oldcitycatering@gmail.com>
2022-03-08 10:58:26 -0500

    deleted component not being used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./index.css";
 import Landing from "./pages/Landing";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
